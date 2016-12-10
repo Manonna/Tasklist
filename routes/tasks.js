c343cee793e6cd36bff58ddf7956e5873ff80881
Manon Vrugt <manonvrugt@gmail.com>
2016-12-10 22:48:59 +0100

    backend finished, still needs testing

--- routes/tasks.js
+++ routes/tasks.js
@@ -27,11 +27,66 @@ router.get('/task/:task_id', (req, res, next) => {
 		where: {
 			id: task
 		}
-	} ).then((thing) => {
-		res.send(thing)
+	} ).then((singletask) => {
+		res.send(singletask)
+	})
+})
+//Save new task
+router.post('/task' (req, res, next) => {
+	let task = req.body.title
+	if (!task.title || (task.finished + '')) {
+		res.status(400)
+		res.send({"error": "Bad Data"})
+	} else {
+		Task.create(task, (err, task) => {
+			if (err) {
+				throw err
+			}
+			res.send(task)
+		})
+	}
+})
+
+//remove task
+router.delete('/task/:task_id', (req, res, next) => {
+	let task = req.params.task_id
+	Task.remove({
+		where: {
+			id: task
+		}
+	}).then(deletedtask => {
+		res.send(deletedtask)
 	})
 })
 
+//edit task
+router.put('/task/:task_id', (req, res, next) => {
+	let task = req.body.title
+	let updTask = {}
+
+	if(task.finished){
+		updTask.finished = task.finished
+	}
+
+	if(task.title){
+		updTask.title = task.title
+	}
+
+	if(!updTask) {
+		res.status(400)
+		res.send({
+			"error": "Bad Data"
+		})
+	} else {
+		Task.set({
+		where: {
+			id: task
+			}, updTask, {}
+		}).then(updatedtask => {
+			res.send(updatedtask)
+			})
+		}	
+})
 
 db.sync( {force: true} ).then( ()=> {
 	Task.create( {
